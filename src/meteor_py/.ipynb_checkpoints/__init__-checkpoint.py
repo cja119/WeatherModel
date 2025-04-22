4af7ea7f3ca9d375f4d9cc1a3e39e624310ff0d6
cja119 <cja119@ic.ac.uk>
2025-04-22 15:33:56 +0100

    Tidied up .gitignore

--- src/meteor_py/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from .demand_profile import DemandProfile
-from .weather_data import WeatherData
-from .renewable_modelling import RenewableEnergy
-
-__all__ = ['DemandProfile', 'WeatherData', 'RenewableEnergy']
