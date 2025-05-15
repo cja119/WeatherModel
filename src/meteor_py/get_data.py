4eb7baadd690920210670b52c1ba4ee2895ae53f
Cameron Aldren <cameron.aldren19@imperial.ac.uk>
2025-05-15 14:40:43 +0100

    added getdata functionality

---
+++ src/meteor_py/get_data.py
@@ -0,0 +1,26 @@
+""" """
+
+from pathlib import Path
+from glob import glob
+
+
+class GetData:
+    def __init__(self, keys):
+        targ = Path(__file__).parent / "data"
+        self._data = []
+        for file_name in keys:
+            csvs_files = glob(targ / file_name)
+
+        for file_name in csvs_files:
+            with open(file_name) as f:
+                next(f)
+                for line in f:
+                    parts = line.strip().split()
+                    if len(parts) == 2:
+                        _, value = parts
+                    elif len(parts) == 1:
+                        value = float(parts[0])
+                    self._data.extend([float(value)])
+
+    def data(self):
+        return self._data
