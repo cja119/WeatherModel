5cbd457c0b9165ab6af9d2a1325686080430439d
Cameron Aldren <cameron.aldren19@imperial.ac.uk>
2025-05-15 14:42:08 +0100

    small update

--- src/meteor_py/__init__.py
+++ src/meteor_py/__init__.py
@@ -1,5 +1,6 @@
 from .demand_profile import DemandProfile
 from .weather_data import WeatherData
 from .renewable_modelling import RenewableEnergy
+from .get_data import GetData
 
-__all__ = ['DemandProfile', 'WeatherData', 'RenewableEnergy']
+__all__ = ['DemandProfile', 'WeatherData', 'RenewableEnergy','GetData']
